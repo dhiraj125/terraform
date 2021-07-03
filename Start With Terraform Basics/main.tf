99c704da1715a60c7318baab52bb259b38967786
Dhiraj Parwani <dhiraj125@gmail.com>
2021-07-03 08:53:38 +0530

    usingCredsinProviderfile

--- Start With Terraform Basics/main.tf
+++ Start With Terraform Basics/main.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-  region     = "us-west-2"
-  access_key = "AKIAY5S26G73RPDPCANY"
-  secret_key = "my-secret-key"
-}
 
 resource "aws_instance" "MyFirstInstnace" {
   ami           = "ami-0dd273d94ed0540c0"
---
+++ Start With Terraform Basics/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIAY5S26G73RPDPCANY"
+  secret_key = "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"
+}
