5e8d60e3e752f4d8dc88fca81830e30ed9de4795
Dhiraj Parwani <dhiraj125@gmail.com>
2021-07-03 13:32:07 +0530

    Variables

--- Start With Terraform Basics/provider.tf
+++ Start With Terraform Basics/provider.tf
@@ -3,3 +3,9 @@ provider "aws" {
   access_key = "AKIAY5S26G73RPDPCANY"
   secret_key = "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"
 }
+
+provider "aws" {
+  region     = "${var.AWS_Region}"
+  access_key = "${var.AWS_Access_Key}"
+  secret_key = "${var.AWS_Secret_Key}"
+}
