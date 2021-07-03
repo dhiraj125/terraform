d26042c9088d3454c1305c2cfc7457044fd016c9
Dhiraj Parwani <dhiraj125@gmail.com>
2021-07-03 13:35:48 +0530

    Variables

---
+++ Variables Detailed Lab/main.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "MyFirstInstnace" {
+  ami           = "ami-0dd273d94ed0540c0"
+  instance_type = "t2.micro"
+  tags = {
+      Name = "Instance created using Variable"
+  }
+}
---
+++ Variables Detailed Lab/variables.tf
@@ -0,0 +1,7 @@
+variable "AWS_Region" {
+  default = "us-west-2"
+}
+
+variable "AWS_Access_Key" {}
+
+variable "AWS_Secret_Key" {}
