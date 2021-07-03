7f53198de79f7f2f4e2990d5d1c48510be7b4545
Dhiraj Parwani <dhiraj125@gmail.com>
2021-07-03 12:48:02 +0530

    MultipleInstances

--- Start With Terraform Basics/main.tf
+++ Start With Terraform Basics/main.tf
@@ -1,5 +1,9 @@
 
 resource "aws_instance" "MyFirstInstnace" {
+  count = 3
   ami           = "ami-0dd273d94ed0540c0"
   instance_type = "t2.micro"
+  tag = {
+      Name = "Instance - ${count.index}"
+  }
 }
