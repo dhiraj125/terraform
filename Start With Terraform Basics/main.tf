8e9a58b23747128b0dbc6dc6cc0e683b176603d5
Dhiraj Parwani <dhiraj125@gmail.com>
2021-07-03 12:50:43 +0530

    tagsinsteadoftag

--- Start With Terraform Basics/main.tf
+++ Start With Terraform Basics/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "MyFirstInstnace" {
   count = 3
   ami           = "ami-0dd273d94ed0540c0"
   instance_type = "t2.micro"
-  tag = {
+  tags = {
       Name = "Instance - ${count.index}"
   }
 }
