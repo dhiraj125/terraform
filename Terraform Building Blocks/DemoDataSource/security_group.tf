1608099922f053b16436f1f56648de4edc265e55
Dhiraj Parwani <dhiraj125@gmail.com>
2021-07-04 09:15:48 +0530

    Security Group Creation

---
+++ Terraform Building Blocks/DemoDataSource/security_group.tf
@@ -0,0 +1,15 @@
+data "aws_ip_ranges" "us_west_ip_ranges" {
+    region=["us-west-1","us-west-2"]
+    resources =["ec2"]
+}
+
+resource "aws_security_group" "sg-custom-security-group" {
+    name="sg-custom-security-group"
+    ingress {
+        from_port = "443"
+        to_port = "443"
+        protocol = "tcp"
+        cidr_blocks = data.aws_ip_ranges.us_west_ip_ranges.cidr_blocks
+    }
+  
+}
