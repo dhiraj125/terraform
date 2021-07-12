b4fe901b7db3fbda5da9335c4b047643275e2ecc
Dhiraj Parwani <dhiraj125@gmail.com>
2021-07-12 13:54:06 +0530

    Modified Variables

--- Terraform Modules/provider.tf
+++ Terraform Modules/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIAY5S26G734E24TOGT"
-  secret_key = ""
-  region     = "us-west-2"
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+  region     = var.AWS_REGION
 }
